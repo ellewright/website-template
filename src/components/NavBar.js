8a24201a99c6c93896a8bbdecd66b19cba4e9f36
Eleanor Wright <elliewright055@gmail.com>
2024-05-13 07:47:48 -0500

    Bug fix

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,7 +3,7 @@ export default function NavBar() {
     return (
         <div className="navbar-container">
             <nav className="flex navbar">
-                <img src='/logo.jpg' />
+                <img src='/logo.jpg' alt='logo' />
                 <ul>
                     <li><Link href="">Home</Link></li>
                     <li><Link href="/about">About</Link></li>
