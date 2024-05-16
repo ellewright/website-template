b6474e5595375120ce3a363103e1a5562f10a0b6
Eleanor Wright <elliewright055@gmail.com>
2024-05-16 17:28:40 -0500

    Begin work on contact page

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -14,6 +14,39 @@ export default function Contact() {
                         <figcaption>If you are or know the rude, shushing man, please call.</figcaption>
                     </figure>
                 </div>
+                <div className="input-field">
+                    <div className="input-header">
+                        <h2>Send us a message; we'll get back to you ASAP.</h2>
+                    </div>
+                    <div className="contact-input flex flex-col items-center">
+                        <div className="flex flex-col">
+                            <label for="name">Name (Required)</label>
+                            <input type="text" placeholder="Name" className="name" id="name" required />
+                        </div>
+                        <div className="flex flex-col">
+                            <label for="email">Email (Required)</label>
+                            <input type="text" placeholder="Email" className="email" id="email" required />
+                        </div>
+                        <div className="flex flex-col">
+                            <label for="phone-number">Phone number (Required)</label>
+                            <input type="tel" placeholder="Phone number" classname="phone-number" id="phone-number" required ></input>
+                        </div>
+                        <div className="flex flex-col">
+                            <label for="inquiries">What are you getting in touch about? (Optional)</label>
+                            <select className="inquiries" id="inquiries">
+                                <option value="general">General inquiry</option>
+                                <option value="press">Press inquiry</option>
+                            </select>
+                        </div>
+                        <div className="flex flex-col">
+                            <label for="message">Your message (Required)</label>
+                            <textarea placeholder="Message" className="message" id="message" required></textarea>
+                        </div>
+                        <div className="flex flex-col items-center justify-center">
+                            <button type="submit" className="submit-button">Send</button>
+                        </div>
+                    </div>
+                </div>
             </main>
         </div>
     )
